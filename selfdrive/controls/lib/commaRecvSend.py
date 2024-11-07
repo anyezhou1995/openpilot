c783618cea85f04b2ceecab26100e163caa73439
unknown <Administrator@lap140482.ORNL.GOV>
2024-11-07 10:14:54 -0500

    revise new func test

--- selfdrive/controls/lib/commaRecvSend.py
+++ selfdrive/controls/lib/commaRecvSend.py
@@ -14,34 +14,33 @@ def main(args):
 	sock_send = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	ticks = 0
 	
-	while True:
-		try:
-			readable, _, _ = select.select([sock_recv], [], [], timeout)
-			if readable:
-				missing = 0
-				comm_flag = True
-				data, addr = sock_recv.recvfrom(1024)
-				#print(data, addr)
-				received_speed, received_accel, received_jerk = struct.unpack('>fff', data)
-				print(f"Received speed info: {received_num1}, accel info: {received_num2}, and jerk info: {received_num3} from {addr}")
-			else:
-				#print(f"Not receiving, using default speed: {default_speed}")
-				missing += 1
-			if missing >=20:
-				comm_flag = False
+    while True:
+        try:
+            readable, _, _ = select.select([sock_recv], [], [], timeout)
+            if readable:
+                missing = 0
+                comm_flag = True
+                data, addr = sock_recv.recvfrom(1024)
+                #print(data, addr)
+                received_speed, received_accel, received_jerk = struct.unpack('>fff', data)
+                print(f"Received speed info: {received_num1}, accel info: {received_num2}, and jerk info: {received_num3} from {addr}")
+            else:
+                #print(f"Not receiving, using default speed: {default_speed}")
+                missing += 1
+            if missing >=20:
+                comm_flag = False
 
-			speed = 10 + np.sin(ticks+0.2)
-			packed_message = struct.pack('>f', speed)
-			sock_send.sendto(packed_message, (TARGET_IP, TARGET_PORT))
-			print(f"Send speed: {speed}")
-			print("Waiting...")
-		except BlockingIOError:
-			#print(f"Not receiving, using default speed: {default_speed}")
-			comm_flag = False
-		except KeyboardInterrupt:
-			break
-			print("\nTransmission interrupted by user.")
-        
+            speed = 10 + np.sin(ticks+0.2)
+            packed_message = struct.pack('>f', speed)
+            sock_send.sendto(packed_message, (TARGET_IP, TARGET_PORT))
+            print(f"Send speed: {speed}")
+            print("Waiting...")
+        except BlockingIOError:
+            #print(f"Not receiving, using default speed: {default_speed}")
+            comm_flag = False
+        except KeyboardInterrupt:
+            break
+            print("\nTransmission interrupted by user.")
         ticks += 1
 		
 	sock_recv.close()
