908ed8f03c1a3bcb213c5a8e4b75ae5516261623
Joe Beck <jbeck6f@gmail.com>
2025-04-07 10:42:35 -0400

    RadarVis 0.1

--- tools/vilTools/radarTools.py
+++ tools/vilTools/radarTools.py
@@ -2,15 +2,61 @@
 import cereal.messaging as messaging
 
 import time
+import numpy as np
+import pickle
+import socket
+
+def send_array(sock, array, ip="192.168.254.14", port=5000):
+    serialized_data = pickle.dumps(array)
+    sock.sendto(serialized_data, (ip, port))
+
+#Example output
+'''
+[ ( trackId = 0,
+    dRel = 1.2,
+    yRel = -0.04,
+    vRel = 0,
+    aRel = nan,
+    yvRel = nan,
+    measured = true ),
+  ( trackId = 1,
+    dRel = 0.48,
+    yRel = -0,
+    vRel = 0,
+    aRel = nan,
+    yvRel = nan,
+    measured = true ),
+  ( trackId = 29,
+    dRel = 16.08,
+    yRel = 16.64,
+    vRel = -0.05,
+    aRel = nan,
+    yvRel = nan,
+    measured = true ) ]
+'''
 
 if __name__ == '__main__':
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     
-    while 1:
-        sm= messaging.SubMaster(['liveTracks'])
-        sm.update(0)
+    sm= messaging.SubMaster(['liveTracks'])
+    try:
+        while 1:
+            sm.update()
+            mes= sm['liveTracks']
+            
+            points= mes.points
+            if len(points) > 0:
+                d= [[points[i].trackId, points[i].dRel, points[i].yRel] for i in range(len(points))]
+                d= np.array(d)
+                print(d)
+            
+            send_array(sock, d)
+            
+            time.sleep(0.05)
+            
+    finally:
+        sock.close()
         
-        print(sm['liveTracks'])
-        
-        time.sleep(1)
     
     
+
--- tools/vilTools/rsync.sh
+++ tools/vilTools/rsync.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 ip=${1#-}
+# First copy the remote logs folder to local machine, deleting existing local copy if it exists
+rsync -avzP --delete comma@$ip:/data/openpilot/tools/vilTools/logs ./local_logs_backup
+# Then sync local tools to remote machine
 rsync -avzP --delete . comma@$ip:/data/openpilot/tools/vilTools
